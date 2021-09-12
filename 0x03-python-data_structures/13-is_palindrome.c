e1b9df34a43d9cb671cb73e8c179d3d591ca3abe
Aficionado01 <emmanuelugochukwu530@gmail.com>
2021-09-12 05:11:58 -0700

    13-is_palindrome.c

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,42 +1,23 @@
-include "lists.h"
+#include "lists.h"
 #include <stdlib.h>
-#include <stdio.h>
-#include <stddef.h>
-
 /**
- * is_palindrome- Checks if a singly linked list is a palindrome.
- * @head: Head pointer
- * Return: 0 if it is not a palindrome, 1 if it is a palindrome.
+ * is_palindrome - check if a list is palindrome
+ * @head: list head pointer
+ * Return: 1 if the list is palindorme or 0 if not.
  */
 int is_palindrome(listint_t **head)
 {
-listint_t *node;
-int values[9999], i = 0, c = 0;
+int elms[2048];
+int len = 0, i;
+listint_t *curr = NULL;
 
-if ((!*head) || (!head))
-{
+if (!head || *head == NULL)
 return (1);
-}
-node = *head;
-if (!node->next)
-{
-return (1);
-}
-while (node)
-{
-values[i] = node->n;
-node = node->next;
-i++;
-}
-i--;
-while (i >= 0 && c <= i)
-{
-if (values[i] != values[c])
-{
+curr = *head;
+for (i = 0; curr && len < 2048; len++, curr = curr->next)
+elms[len] = curr->n;
+for (i = 0; i <= len / 2; i++)
+if (elms[i] != elms[len - i - 1])
 return (0);
-}
-i--;
-c++;
-}
 return (1);
 }
