fb2449199ac918a39f44e8954e734bce023675c3
Aficionado01 <emmanuelugochukwu530@gmail.com>
2021-12-13 18:29:51 -0800

    6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 const multiLine = [
-    'C is fun',
-    'Python is cool',
+  'C is fun',
+  'Python is cool',
   'JavaScript is amazing'
 ];
 
 for (let i = 0; i < multiLine.length; i++) {
-    console.log(multiLine[i]);
+  console.log(multiLine[i]);
 }
