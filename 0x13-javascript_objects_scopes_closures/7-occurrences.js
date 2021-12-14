02e0f714d87b270d977bae0133a799007e915712
Aficionado01 <emmanuelugochukwu530@gmail.com>
2021-12-14 10:39:25 -0800

    7-occurrences.js

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,10 +1,11 @@
 #!/usr/bin/node
 exports.nbOccurences = function (list, searchElement) {
-  let number = 0;
-  for (const item of list) {
-    if (item === searchElement) {
-	    number++;
+  let count = 0;
+  list.sort((a, b) => a - b);
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] === searchElement) {
+	    count++;
     }
   }
-   return number;
+  return (count);
 };
