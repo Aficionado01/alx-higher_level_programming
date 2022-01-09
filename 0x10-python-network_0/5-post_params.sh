a3bed68408287cf8a5e696c381bbb60882ee190e
Aficionado01 <emmanuelugochukwu530@gmail.com>
2022-01-09 12:40:17 -0800

    5-post_params.sh

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sends POST request and displays body response
-curl -sX "POST" "$1" -d "email=hr@holbertonschool.com&subject=I will always be here for PLD"
+#curl sends POST req to URL,display response body
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here PLD" "$1"
