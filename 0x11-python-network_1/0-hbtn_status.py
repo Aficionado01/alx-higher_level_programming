60e5f04dfc5d1a97cc836c971af84836500a5a29
Aficionado01 <emmanuelugochukwu530@gmail.com>
2022-01-09 15:52:03 -0800

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3
-# Module with code that fetches a URL
+# Fetches https://intranet.hbtn.io/status
 import urllib.request
-
-
-if __name__ == "__main__":
-    # fetches a URL
-    url = 'https://intranet.hbtn.io/status'
-    with urllib.request.urlopen(url) as response:
-        html = response.read()
+req = urllib.request.Request('https://intranet.hbtn.io/status')
+with urllib.request.urlopen(req) as response:
+    data = response.read()
     print("Body response:")
-    print("\t- type: {}".format(type(html)))
-    print("\t- content: {}".format(html))
-    print("\t- utf8 content: {}".format(html.decode("utf-8")))
+    print("\t- type: {}".format(type(data)))
+    print("\t- content: {}".format(data))
+    print("\t- utf8 content: {}".format(data.decode('utf8')))
