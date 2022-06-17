5c68641aa072b0febadbec29c06c4a949b9a5965
Aficionado01 <emmanuelugochukwu530@gmail.com>
2022-06-17 14:30:51 +0000

    4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,15 +1,14 @@
 #!/usr/bin/node
 const request = require('request');
 
-request(process.argv[2], (err, _, bdy) => {
-      if (err) return err;
-      const result = JSON.parse(bdy).results;
+const url = process.argv[2];
+request(url, function (err, resp, body) {
+      if (err) throw err;
       let count = 0;
-      const character = 'https://swapi-api.hbtn.io/api/people/18/';
-      for (let i = 0; i < result.length; i++) {
-	      for (let j = 0; j < result[i].characters.length; j++) {
-		        if (result[i].characters[j] === character) {
-			            count += 1;
+      for (const film of JSON.parse(body).results) {
+	      for (const character of film.characters) {
+		        if (character.includes('18')) {
+			            count++;
 			          }
 		      }
 	    }
